479125690f6f8fa1ce947d660411b761930d498b
unknown <you@example.com>
2025-06-08 15:54:36 -0300

    cambios en pagos

---
+++ ManagerPago.h
@@ -0,0 +1,24 @@
+#ifndef MANAGERPAGO_H_INCLUDED
+#define MANAGERPAGO_H_INCLUDED
+#include "ArchivoPagos.h"
+
+class ManagerPago{
+
+public:
+    void agregarPago();
+    void leerArchivo();
+    void buscarPagoPorCodigo();
+    void listarPagosDelMes();
+    Pagos obtenerPagoPorCodigo(int codPago);
+};
+
+
+
+
+
+
+
+
+
+
+#endif // MANAGERPAGO_H_INCLUDED
